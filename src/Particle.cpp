8f94805074bdb7b80dfb678d723af4d655a1799b
AJM432 <49791407+AJM432@users.noreply.github.com>
2023-03-04 18:24:26 -0500

    first commit

---
+++ src/Particle.cpp
@@ -0,0 +1,72 @@
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
+#include <Particle.hpp>
+#include <Global.hpp>
+#include <Helper.hpp>
+
+// default class
+// used to create random particles
+Particle::Particle()
+{
+	radius=HelperFunc::randNum(std::rand(), MIN_RADIUS, MAX_RADIUS);
+	pos=sf::Vector2f(HelperFunc::randNum(std::rand(), 0, (int)(WIDTH-2*radius)), 
+			HelperFunc::randNum(std::rand(), 0, (int)(HEIGHT-2*radius)));
+
+	vel=sf::Vector2f(HelperFunc::randNum(std::rand(), 0, MAX_VELOCITY), 
+			HelperFunc::randNum(std::rand(), 0, MAX_VELOCITY));
+
+	acc=sf::Vector2f(0, 0);
+	mass=1;
+	shape.setRadius(radius);
+	sf::Color color(radius*(255/MAX_RADIUS), vel.x*(255/MAX_VELOCITY), vel.y*(255/MAX_VELOCITY)); // red
+	shape.setFillColor(color);
+}
+
+Particle::Particle(sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f acc, double radius, double mass, sf::Color color)
+{
+	pos=pos;
+	vel=vel;
+	acc=acc;
+	radius=radius;
+	mass=mass;
+	shape.setFillColor(color);
+	shape.setRadius(radius);
+}
+
+
+void Particle::check_wall_collision(sf::RenderWindow& window, double delta_time) {
+
+	if (pos.x  <= 0) {
+		vel.x *= -1;
+		pos.x=0;
+
+	}
+	if (pos.x + 2*shape.getRadius() >= WIDTH) {
+		vel.x *= -1;
+		pos.x = WIDTH-shape.getRadius()*2;
+	}
+
+	if (pos.y <= 0) {
+		vel.y *= -1;
+		pos.y = 0;
+	}
+
+	if (pos.y + 2*shape.getRadius() >= HEIGHT) {
+		vel.y *= -1;
+		pos.y = HEIGHT-shape.getRadius()*2;
+	}
+}
+
+void Particle::update(sf::RenderWindow& window, double delta_time)
+{
+	check_wall_collision(window, delta_time);
+
+	pos.x = pos.x + vel.x*delta_time;
+	pos.y = pos.y + vel.y*delta_time;
+
+	shape.setPosition(pos);
+
+	window.draw(shape);
+}
---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+#include <Particle.hpp>
+#include <Global.hpp>
+#include <type_traits>
+#include <Helper.hpp>
+
+
+int main()
+{
+	sf::Clock clock;
+	std::vector<Particle> particle_array(NUM_PARTICLES);
+
+	std::srand(time(NULL)); // init rand seed
+
+	// creates particle array
+	for (int i=0; i < NUM_PARTICLES; i++){
+		Particle p;
+		particle_array.push_back(p); 
+	}
+
+	// creates mouse shape
+	Particle mouseShape(sf::Vector2f(0, 0), sf::Vector2f(0, 0), sf::Vector2f(0, 0), 10, 1, sf::Color::White);
+
+
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Particle Fluid Simulation");
+	window.setFramerateLimit(FPS);
+
+
+	while (window.isOpen())
+	{
+
+		sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		sf::Time delta_time = clock.restart();
+
+		for (int i=0; i < NUM_PARTICLES; i++) {
+			particle_array[i].update(window, delta_time.asSeconds());
+		}
+		mouseShape.pos.x = localPosition.x;
+		mouseShape.pos.y=  localPosition.y-mouseShape.radius;
+		std::cout << localPosition.y << std::endl;
+		mouseShape.update(window, delta_time.asSeconds());
+		window.display();
+	}
+
+	return 0;
+}
